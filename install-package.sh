96cd8a96f63ccc5aac7d080e367456620f44f586
Vinayak Kankanwadi <vinayakkankanwadi@hotmail.com>
2024-11-08 16:34:15 +1000

    Update install-package.sh

--- install-package.sh
+++ install-package.sh
@@ -27,13 +27,8 @@ sudo usermod -aG docker ubuntu
 sudo curl -L "https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-
-# Install WireGuard and WireGuard Tools
-sudo apt-get install -y wireguard wireguard-tools
-
 # Verify installations
 docker --version
 docker-compose --version
-wg --version
 
-echo "Docker, Docker Compose, WireGuard, and WireGuard Tools installation complete."
+echo "Docker and Docker Compose installation complete."
